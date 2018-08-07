706a7c94834a6e1491afc6d70cb52bb4f16362f2
SeenivasanSeeni <seeni0424@gmail.com>
2018-08-07 13:43:23 +0530

    change background and remove some text

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+
 var icons=["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"]; 
 var stars=document.getElementsByClassName("stars")[0];
     
@@ -91,9 +89,9 @@ function click(e){
 
 
 function init(){
-    //icons_=shuffle(icons);
-    icons_=icons; //for debugging
-    console.log("rebuilding the framework");
+    icons_=shuffle(icons);
+    //icons_=icons; //for debugging
+    console.log("rebuilding the deck");
     stars.innerHTML="";
     deck.innerHTML="";
     moves=0;
@@ -110,13 +108,3 @@ function init(){
 var restartContainer=document.getElementsByClassName("restart")[0];
 restartContainer.addEventListener("click",init);
 init()
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
